2ba3a6008d28f17b859cc22febacc4313a436589
Derek Cheng <81448549+Hytherist@users.noreply.github.com>
2023-05-09 21:01:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const { Client, GatewayIntentBits } = require('discord.js')
+const Discord = require('discord.js')
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 require('dotenv/config')
 
 // const remind = require('./functionalities/reminder');
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-    ],
-})
+client.login(process.env.TOKEN)
 
 client.on('ready', () => {
     console.log('bot is ready')
@@ -21,5 +16,3 @@ client.on('messageCreate', message => {
         message.reply('pong')
     } 
 })
-
-client.login(process.env.TOKEN)
