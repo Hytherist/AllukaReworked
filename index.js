029895e2139441fbec31c925601bd8b88a724c5e
Derek Cheng <81448549+Hytherist@users.noreply.github.com>
2023-05-09 20:54:55 -0700

    push back

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Client, GatewayIntentBits } = require('discord.js')
 require('dotenv/config')
 
+// const remind = require('./functionalities/reminder');
+
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -11,14 +13,13 @@ const client = new Client({
 
 client.on('ready', () => {
     console.log('bot is ready')
+    remind(client);
 })
 
 client.on('messageCreate', message => {
     if (message.content === 'ping') {
         message.reply('pong')
-    }
+    } 
 })
 
 client.login(process.env.TOKEN)
-
-/* hi */
